741c4d343dd7ce76abfed534c1b13f1c31d6c4f6
Kudav5 <kdvalerian@gmail.com>
2022-10-25 10:45:16 +0700

    script

---
+++ tugas.py
@@ -0,0 +1,22 @@
+'''
+buat kode program untuk 
+menghitung luas dan
+keliling lingkaran
+menggunakan python
+'''
+from math import pi
+
+r = input("jari-jari linkaran =...")
+d = input("diameter linkaran =...")
+a = input("pilih keliling atau luas? ")
+float(r)
+float(d)
+pi = 3,14
+
+if a =='keliling':
+
+    k = 2 * pi * r
+elif a =='luas':
+    l = pi * r * r
+
+
