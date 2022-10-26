35c1b70f73f9d10b1f82717fbe80b11b1deb18ea
Kudav5 <kdvalerian@gmail.com>
2022-10-26 10:24:46 +0700

    sccript

--- tugas.py
+++ tugas.py
@@ -9,14 +9,18 @@ from math import pi
 r = input("jari-jari linkaran =...")
 d = input("diameter linkaran =...")
 a = input("pilih keliling atau luas? ")
-float(r)
-float(d)
-pi = 3,14
+r = int(r)
+d = int(d)
+pi = 3.14
 
 if a =='keliling':
-
-    k = 2 * pi * r
+    b = input("dengan r atau d? ")
+    if b =='r':
+        k = 2 * pi * r
+        print("hasil keliling lingkaran adalah", k)
+    elif b =='d':
+        k = pi * d
+        print("hasil keliling lingkaran adalah", k)
 elif a =='luas':
     l = pi * r * r
-
-
+    print("hasil luas lingkaran adalah: ", l)
